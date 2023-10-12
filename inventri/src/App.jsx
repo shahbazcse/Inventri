ff29f31c19a2dc255f507152a832bdd59f329afd
shahbazcse <cseshahbaz@gmail.com>
2023-10-12 23:09:12 +0530

    Designed NavBar

--- inventri/src/App.jsx
+++ inventri/src/App.jsx
@@ -10,10 +10,8 @@ function App() {
   return (
     <div className="App">
       <div className="flex h-screen">
-        <div className="bg-red-400 w-[15%]">
-          <NavBar />
-        </div>
-        <div className="bg-green-400 w-[85%]">
+        <NavBar />
+        <div className="bg-blue-100 w-[85%]">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/inventory" element={<Inventory />} />
--- inventri/src/components/NavBar.jsx
+++ inventri/src/components/NavBar.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const isActive = (path) => {
+    return path === window.location.pathname;
+  };
+
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-      <Link to="/">Dashboard</Link>
-      <Link to="/inventory">Inventory</Link>
-      <Link to="/sales">Sales</Link>
-      <Link to="/analytics">Analytics</Link>
+    <div className="flex flex-col gap-4 mt-12 items-center w-[15%] bg-[#FFFFFF]">
+      <div
+        className={`${
+          isActive("/") ? "bg-blue-300" : "hover:bg-blue-200"
+        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold`}
+        onClick={() => navigate("/")}
+      >
+        Dashboard
+      </div>
+      <div
+        className={`${
+          isActive("/inventory") ? "bg-blue-300" : "hover:bg-blue-200"
+        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold`}
+        onClick={() => navigate("/inventory")}
+      >
+        Inventory
+      </div>
+      <div
+        className={`${
+          isActive("/sales") ? "bg-blue-300" : "hover:bg-blue-200"
+        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold`}
+        onClick={() => navigate("/sales")}
+      >
+        Sales
+      </div>
+      <div
+        className={`${
+          isActive("/analytics") ? "bg-blue-300" : "hover:bg-blue-200"
+        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold`}
+        onClick={() => navigate("/analytics")}
+      >
+        Analytics
+      </div>
     </div>
   );
 }
