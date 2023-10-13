39f30a120c21377cf6d0097926c25e8f202ab4f6
shahbazcse <cseshahbaz@gmail.com>
2023-10-13 19:11:34 +0530

    Added NavBar Icons

--- inventri/src/components/NavBar.jsx
+++ inventri/src/components/NavBar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaShopware } from "react-icons/fa";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { BsBoxes } from "react-icons/bs";
+import { PiShoppingCartSimpleLight } from "react-icons/pi";
+import { VscGraph } from "react-icons/vsc";
 
 function NavBar() {
   const navigate = useNavigate();
@@ -9,38 +14,46 @@ function NavBar() {
   };
 
   return (
-    <div className="flex flex-col gap-4 mt-12 items-center w-[15%] bg-[#FFFFFF] drop-shadow-md">
+    <div className="flex flex-col gap-4 items-center w-[15%] bg-[#FFFFFF] drop-shadow-md">
+      <div className="flex items-center justify-center gap-2 mt-6 mb-12 text-3xl font-bold">
+        <FaShopware />
+        <div>Inventri</div>
+      </div>
       <div
         className={`${
           isActive("/") ? "bg-blue-300" : "hover:bg-blue-200"
-        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold drop-shadow-md`}
+        } w-[16vh] flex items-center gap-4 text-center px-4 py-3 rounded-xl cursor-pointer drop-shadow-md`}
         onClick={() => navigate("/")}
       >
-        Dashboard
+        <LuLayoutDashboard className="h-7 w-7" />
+        <div className="font-bold">Dashboard</div>
       </div>
       <div
         className={`${
           isActive("/inventory") ? "bg-blue-300" : "hover:bg-blue-200"
-        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold drop-shadow-md`}
+        } w-[16vh] flex items-center gap-4 text-center px-4 py-3 rounded-xl cursor-pointer drop-shadow-md`}
         onClick={() => navigate("/inventory")}
       >
-        Inventory
+        <BsBoxes className="h-7 w-7" />
+        <div className="font-bold">Inventory</div>
       </div>
       <div
         className={`${
           isActive("/sales") ? "bg-blue-300" : "hover:bg-blue-200"
-        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold drop-shadow-md`}
+        } w-[16vh] flex items-center gap-4 text-center px-4 py-3 rounded-xl cursor-pointer drop-shadow-md`}
         onClick={() => navigate("/sales")}
       >
-        Sales
+        <PiShoppingCartSimpleLight className="h-7 w-7" />
+        <div className="font-bold">Sales</div>
       </div>
       <div
         className={`${
           isActive("/analytics") ? "bg-blue-300" : "hover:bg-blue-200"
-        } w-[16vh] text-center px-4 py-3 rounded-xl cursor-pointer font-bold drop-shadow-md`}
+        } w-[16vh] flex items-center gap-4 text-center px-4 py-3 rounded-xl cursor-pointer drop-shadow-md`}
         onClick={() => navigate("/analytics")}
       >
-        Analytics
+        <VscGraph className="h-7 w-7" />
+        <div className="font-bold">Analytics</div>
       </div>
     </div>
   );
--- inventri/src/pages/Dashboard.jsx
+++ inventri/src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import MetricCard from "../components/MetricCard";
+import img1 from "../assets/01.svg";
+import img2 from "../assets/02.svg";
+import img3 from "../assets/03.svg";
+import img4 from "../assets/04.svg";
 
 function Dashboard() {
   return (
@@ -8,11 +13,19 @@ function Dashboard() {
       </div>
       <div className="flex flex-col gap-6 items-center justify-center">
         <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-          beatae temporibus voluptates similique assumenda, qui non odio impedit
-          consequatur quibusdam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          eius voluptatum atque fuga hic et nihil, pariatur sed eos provident,
+          illo rem amet aperiam impedit temporibus doloribus neque aliquid. At
+          laudantium in officiis natus nesciunt quo provident exercitationem.
+          Maxime similique magni alias accusamus omnis eos est ducimus suscipit
+          autem ratione!
         </div>
-        <div>Content</div>
+        <div className="flex flex-wrap items-center justify-center my-12 mx-48 gap-12">
+          <MetricCard label={"Total Stocks"} value={"100"} bannerImg={img1} />
+          <MetricCard label={"Total Sales"} value={"100"} bannerImg={img2} />
+          <MetricCard label={"Total Revenue"} value={"100"} bannerImg={img3} />
+          <MetricCard label={"Last Sale"} value={"100"} bannerImg={img4} />
+        </div>
       </div>
     </div>
   );
